894cd98ef2c96f21b8943d955cd4f7948c1adb8c
Christian Burdea <burdear64@zedat.fu-berlin.de>
2021-05-03 23:38:40 +0200

    added arguments to parser

--- src/vsp/arguments.py
+++ src/vsp/arguments.py
@@ -6,7 +6,7 @@ def args():
     parser.add_argument('-b', '--BATCH', default=64)
     parser.add_argument('-b', '--N_EPOCHS', default=1)
     parser.add_argument('--LR', default=3e-4)
-    parser.add_argument('--N_ROLLOUT', default=10)
+    parser.add_argument('--N_ROLLOUT', default=20)
     parser.add_argument('--ROLLOUT_STEPS', default=10)
     parser.add_argument('--N_STEPS', default=200)
     parser.add_argument('--init_T', default=100.0)
