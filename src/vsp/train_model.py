8394b96cc3bc4620683cf37296adc96f46087c83
Christian Burdea <burdear64@zedat.fu-berlin.de>
2021-05-04 00:30:47 +0200

    added gpu parallelism

--- src/vsp/train_model.py
+++ src/vsp/train_model.py
@@ -43,9 +43,7 @@ if __name__ == "__main__":
     print('Data loaded: ', stop - start)
 
     model = Model(input_node_dim=8, hidden_node_dim=64, input_edge_dim=2, hidden_edge_dim=16)
-    model = model.to(device)
-    model = nn.DataParallel(model, device_ids=[0,1])
-    model.to(f'cuda:{model.device_ids[0]}')
+    net = torch.nn.DataParallel(model, device_ids=[0,1], output_device=device).to(device=device)
 
     #model.load_state_dict(torch.load("model/v8-tw-iter200-rm25-latest_custom.model"))
 
